0ee05ff1e0a6320ed47e9817c3b2db8742d8e60e
hugo_wang <hugo_wang@goglobal.com.tw>
2017-04-06 15:54:57 +0800

    about centos 6.9 install python3 and mysqlclient script

---
+++ py3_install.sh
@@ -0,0 +1,7 @@
+sudo yum update
+sudo yum install -y https://centos6.iuscommunity.org/ius-release.rpm
+sudo yum update
+sudo yum install -y python35u python35u-pip python35u-devel
+sudo yum install mysql-devel
+sudo yum install gcc
+sudo pip3.5 install mysqlclient
